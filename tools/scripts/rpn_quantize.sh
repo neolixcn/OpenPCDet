59998de0fda32c8ff328546460fd9e49eb5a19e8
chrisHuxi <chris_huxi@163.com>
2021-06-09 14:53:38 +0800

    add bash script

---
+++ tools/scripts/rpn_quantize.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -e #任意 command fails 触发
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+#step1. 检查模型
+cd ../
+source activate torch1.3.1
+pwd
+pwddir=${PWD} #tools
+parentdir="$(dirname "${pwddir}")" #pcdet
+eval_or_calib=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
+    python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["eval_or_calib"])'`;
+
+echo $eval_or_calib
+if [ "$eval_or_calib" = "eval" ]; then
+  python test_evaluate_cpp_result.py --cfg_file cfgs/neolix_models/pointpillar_1022_for_calib.yaml --ckpt /home/songhongli/1274_pcdet/checkpoint_pth/checkpoint_epoch_80_1022.pth --batch_size 1 --workers 1
+else
+  #step2. 生成量化表
+  # optional: 选择校准数据集，如果之后的数据集没有变化，这一步骤运行时输入除了 ‘y’之外的任意键即可
+  python $pwddir/calib_utils/cal_data_distrib.py
+  python $pwddir/calib_utils/mv_calib_datasets.py
+
+  copied_calib_pc_dir=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
+      python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["copied_calib_pc_dir"])'`;
+
+  # --------- 清理掉之前的文件 ---------
+  calib_rpn_input_dir=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
+      python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["calib_rpn_input_dir"])'`;
+  echo $calib_rpn_input_dir
+  rm $calib_rpn_input_dir/*.*
+  # --------^ 清理掉之前的文件 ^--------
+
+  python demo_for_calib.py --cfg_file cfgs/neolix_models/pointpillar_1022_for_calib.yaml --ckpt /home/songhongli/1274_pcdet/checkpoint_pth/checkpoint_epoch_80_1022.pth --data_path $copied_calib_pc_dir --output_path /home/songhongli/1274_pcdet/infer_out_useless/
+
+  python $pwddir/calib_utils/generate_calib_list.py
+
+  python $pwddir/onnx_utilis/export_vfe_weight.py
+
+  vfe_exported_weight_file=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
+      python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["vfe_exported_weight_file"])'`;
+
+  rpn_tarfile_name=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
+      python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["rpn_tarfile_name"])'`;
+
+  echo "ready to scp to xavier: check the file:"
+  echo $rpn_tarfile_name
+  echo $vfe_exported_weight_file
+
+  echo "done!"
+fi
