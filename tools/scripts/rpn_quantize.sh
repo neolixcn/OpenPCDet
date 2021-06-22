7a56810aef3b30c025d8a9281f6ad7646b288683
chrisHuxi <chris_huxi@163.com>
2021-06-22 10:11:22 +0800

    ready to merge into master branch

--- tools/scripts/rpn_quantize.sh
+++ tools/scripts/rpn_quantize.sh
@@ -16,9 +16,16 @@ parentdir="$(dirname "${pwddir}")" #pcdet
 eval_or_calib=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
     python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["eval_or_calib"])'`;
 
+dataset_yaml_file=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
+    python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["dataset_yaml_file"])'`;
+
+checkpoint_pth=`cat $parentdir/pcdet/pointpillar_quantize_config/rpn_quantize_config.json | \
+    python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["checkpoint_pth"])'`;
+
+
 echo $eval_or_calib
 if [ "$eval_or_calib" = "eval" ]; then
-  python test_evaluate_cpp_result.py --cfg_file cfgs/neolix_models/pointpillar_1022_for_calib.yaml --ckpt /home/songhongli/1274_pcdet/checkpoint_pth/checkpoint_epoch_80_1022.pth --batch_size 1 --workers 1
+  python test_evaluate_cpp_result.py --cfg_file cfgs/neolix_models/$dataset_yaml_file --ckpt $checkpoint_pth --batch_size 1 --workers 1
 else
   #step2. 生成量化表
   # optional: 选择校准数据集，如果之后的数据集没有变化，这一步骤运行时输入除了 ‘y’之外的任意键即可
@@ -35,7 +42,7 @@ else
   rm -rf $calib_rpn_input_dir/*.*
   # --------^ 清理掉之前的文件 ^--------
 
-  python demo_for_calib.py --cfg_file cfgs/neolix_models/pointpillar_1022_for_calib.yaml --ckpt /home/songhongli/1274_pcdet/checkpoint_pth/checkpoint_epoch_80_1022.pth --data_path $copied_calib_pc_dir --output_path /home/songhongli/1274_pcdet/infer_out_useless/
+  python demo_for_calib.py --cfg_file cfgs/neolix_models/$dataset_yaml_file --ckpt $checkpoint_pth --data_path $copied_calib_pc_dir
 
   python $pwddir/calib_utils/generate_calib_list.py
 
